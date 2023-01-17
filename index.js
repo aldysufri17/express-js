e5da0e29e6cf43b894f4fa9f9d2437d1d8768360
aldisu30 <aldisu30@gmail.com>
2023-01-17 11:16:41 +0700

    implement dotEnv

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+require('dotenv').config();
 const routeProducts = require('./src/routes/ProductsRoute.js');
 const middlewareLog = require('./src/middleware/logs');
 const app = express();
-const port = 4000;
-
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 // Middleware
--- src/configs/db.js
+++ src/configs/db.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
 const pool = mysql.createPool({
         host : 'localhost',
         user : 'root',
         password : 'hwhwhwlol',
         database : 'crud'
 });
-
+// host : process.env.DB_HOST || 'localhost',
+// user : process.env.DB_USER ||'root',
+// password : process.env.DB_PASSWORD ||'hwhwhwlol',
+// database : process.env.DB_NAME ||'crud'
 pool.on('error', (err) => {
     console.log(err);
 });
